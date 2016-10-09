961dfd98caa779412fd6240ee4e481d7ddec0870
Whatyson Neves <whatyson@promasters.net.br>
2016-10-09 16:43:00 -0300

    Usando javascript
    Exemplo prático usando o javascript.

---
+++ javascript/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Cotação da ProMasters com javascript</title>
+</head>
+<body>
+
+	<div class="container">
+		O dólar hoje está em <span></span>
+	</div>
+
+	<script language="javascript" type="text/javascript" src="cotacao.js"></script>
+
+</body>
+</html>
