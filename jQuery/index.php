f25186eda4c87c2d4a357e55d369d9a3c439ee72
Whatyson Neves <whatyson@promasters.net.br>
2016-10-09 15:45:21 -0300

    Usando jQuery
    Exemplo prático usando o jQuery.

---
+++ jQuery/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Cotação da ProMasters com jQuery</title>
+</head>
+<body>
+
+	<div class="container">
+		O dólar hoje está em <span></span>
+	</div>
+
+	<script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script language="javascript" type="text/javascript" src="cotacao.js"></script>
+
+</body>
+</html>
